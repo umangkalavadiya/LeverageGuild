d4ee878891252ac4422451b28780ff926907ba84
Nency <snency16@gmail.com>
2023-02-09 18:21:34 +0530

    final commit

--- sprofile/views.py
+++ sprofile/views.py
@@ -4,35 +4,48 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views import View
-from django.urls import reverse
 from .forms import *
 from .models import Sprofile
+from .forms import ProfileForm
+
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
-class EditProfile(View):
-    profile = None
+class ProfileView(View):
+    edit_profile = None
 
     def dispatch(self, request, *args, **kwargs):
-        self.profile, __ = Sprofile.objects.get_or_create(user=request.user)
-        return super(EditProfile, self).dispatch(request, *args, **kwargs)
+        self.edit_profile, __ = Sprofile.objects.get_or_create(user=request.user)
+        return super(ProfileView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        context = {'edit_profile': self.edit_profile, 'segment': 'edit_profile'}
+        return render(request, 'edit_profile.html', context)
+
+    def post(self, request):
+        form = ProfileForm(request.POST, request.FILES, instance=self.edit_profile)
+
+        if form.is_valid():
+            edit_profile = form.save()
+           
+
+            messages.success(request, 'Profile saved successfully')
+            return redirect('/edit_profile')
+        else:
+            messages.error(request, form_validation_error)
+            
+            
+        context = {'edit_profile': self.edit_profile, 'form': form, 'segment': 'edit_profile'}
+        return render(request, 'edit_profile.html', context)
 
 
 
 def edit_profile(request):
     profile, created = Sprofile.objects.get_or_create(user=request.user)
-    user_form = ProfileForm(instance=request.user)
-    sprofile_form = EditProfileForm(instance=profile)
-
     if request.method == 'POST':
-        user_form = ProfileForm(request.POST, instance=request.user)
-        sprofile_form = EditProfileForm(request.POST, instance=profile)
-
-        if user_form.is_valid() and sprofile_form.is_valid():
-            
-            user_form.save()
-            sprofile_form.save()
-            return redirect('/edit_profile/')
-
-    context = {'user_form': user_form, 'sprofile_form': sprofile_form}
-    return render(request, 'edit_profile.html', context)
-
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('/edit_profile')
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'edit_profile.html', {'form': form})
